d041ba3a1441967291fae9fc88cfff75f7addb68
Lara Kobilke <100684892+LKobilke@users.noreply.github.com>
2023-07-01 12:47:45 +0200

    second commit

---
+++ R/createOpenSciPackage.R
@@ -0,0 +1,99 @@
+#' Create a new open science R package
+#'
+#' This function creates a new open science R package with a standardized structure and setup.
+#' It also prompts the user to enter some basic information about the package and the author.
+#'
+#' @param package_name The name of the package to create.
+#' @param path The directory in which to create the package. Defaults to the current working directory.
+#'
+#' @return A message indicating the package was created.
+#' @export
+#' @examples
+#' \dontrun{
+#' createOpenSciPackage("myPackage", path = "/myPath")
+#' }
+createOpenSciPackage <- function(package_name, path = NULL) {
+
+  # Create the new package
+  usethis::create_package(path = file.path(path, package_name))
+
+  # Change the working directory to the new package
+  setwd(file.path(path, package_name))
+
+  # Prompt user for information
+  author_name <- readline(prompt = "Enter the author's name: ")
+  author_email <- readline(prompt = "Enter the author's email: ")
+  description_input <- readline(prompt = "Enter a first description of your package: ")
+
+  # Add DESCRIPTION entries
+  usethis::use_description(fields = list(
+    'Title' = 'Open Science Template Package',
+    'Author' = author_name,
+    'Maintainer' = paste(author_name, '<', author_email, '>', sep = ''),
+    'Description' = description_input,
+    'License' = 'MIT',
+    'Imports' = 'learnr',
+    'Encoding' = 'UTF-8',
+    'LazyData' = 'true'
+  ))
+
+  # Create core R scripts
+  file.create("R/data_preparation.R")
+  file.create("R/data_analysis.R")
+
+  # Add content to R scripts
+  writeLines(c("#' Data preparation function",
+               "#' @export",
+               "data_preparation <- function() {",
+               "  # Add data preparation code here",
+               "}"),
+             "R/data_preparation.R")
+
+  writeLines(c("#' Data analysis function",
+               "#' @export",
+               "data_analysis <- function() {",
+               "  # Add data analysis code here",
+               "}"),
+             "R/data_analysis.R")
+
+  # Use roxygen2 for documentation
+  usethis::use_roxygen_md()
+
+  # Add a README
+  usethis::use_readme_md()
+
+  # Prompt user for license choice
+  cat("Common licenses:\n",
+      "1: MIT (A permissive license that is short and to the point)\n",
+      "2: GPL-3 (A widely-used license that requires derivatives be open source)\n",
+      "3: Apache-2 (A permissive license that also provides an express grant of patent rights)\n",
+      "4: CC0 (Dedicates code into the public domain, or otherwise grants permission to use it for any purpose)\n")
+  license_choice <- as.integer(readline(prompt = "Enter the number for your license choice: "))
+
+  # Add the chosen license
+  if (license_choice == 1) {
+    usethis::use_mit_license()
+  } else if (license_choice == 2) {
+    usethis::use_gpl3_license()
+  } else if (license_choice == 3){
+    usethis::use_apache_license()
+  } else if (license_choice == 4) {
+    usethis::use_cc0_license()
+  } else {
+    cat("Invalid choice. Defaulting to MIT license.\n")
+    usethis::use_mit_license()
+  }
+
+  # Create custom directories
+  dir.create("data")
+  dir.create("data-raw")
+  dir.create("inst")
+  dir.create("inst/sandbox")
+  dir.create("tests")
+
+  # Install required packages
+  usethis::use_package("learnr")
+
+  # Return a message indicating the package was created
+  return("Open Science Template Package created successfully")
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+
+# opensciPackR
+
+The goal of opensciPackR is to facilitate open science by providing a templated R package for structured research data distribution, management, and analysis. This package allows researchers to package their data and analysis code, ensuring reproducibility, transparency, and accessibility of research findings. By using opensciPackR, researchers can easily create custom R packages for their research projects, share them on GitHub, and collaborate with other researchers.
+
+## Installation
+
+You can install the development version of opensciPackR from [GitHub](https://github.com/LKobilke/opensciPackR) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("https://github.com/LKobilke/opensciPackR")
+```
+
+Please note that opensciPackR is currently under development, so the above installation installs the development version from GitHub.
+
+## Example
+Here's a basic example that demonstrates the main function of opensciPackR:
+
+``` r
+library(opensciPackR)
+
+# Create a new opensciPackR project
+createOpenSciPackage("MyResearchPackage")
+```
+
+## Licence
+This project is licensed under the MIT License.
